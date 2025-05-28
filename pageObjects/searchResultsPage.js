10a2916513a9cde2179128fe64187c54e6b11b7b
SruthiKrishnan23 <sruthi.krishnan2696@gmail.com>
2025-05-28 15:29:19 +0100

    updating locators

--- pageObjects/searchResultsPage.js
+++ pageObjects/searchResultsPage.js
@@ -1,7 +1,9 @@
+const {expect}=require('@playwright/test');
 class SearchResultsPage {
   constructor(page) {
     this.page = page;
-    this.moshiMoshiBuyButton = page.getByRole('link', { name: 'Moshi Moshi Moshi Moshi  COVERAGE Japan  DATA 1 GB  VALIDITY 7 Days PRICE 4.' }).getByRole('button');
+    //this.moshiMoshiBuyButton = page.getByRole('link', { name: 'Moshi Moshi Moshi Moshi  COVERAGE Japan  DATA 1 GB  VALIDITY 7 Days PRICE 4.' }).getByRole('button');
+    this.moshiMoshiBuyButton =page.locator('[data-testid="esim-button"]').nth(1)
   }
 
   async clickMoshiMoshiBuyNow() {
